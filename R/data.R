2e459ba0e1986c248f412fd4458a37ba83b4d5bf
liamhaller <liamhllr2@gmail.com>
2023-06-01 20:39:45 +0200

    data

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' league Baseline.
+#'
+#' The original baseline data for league standings
+#'
+#' @format A list containing eight dataframs each with: \code{artist} and
+#'   \code{artist_code}.
+"league_baseline.rda"
